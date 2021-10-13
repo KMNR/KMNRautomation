e688725e3b0ae1c15477dd4c11caf7a1516ca4d9
Caleb-R0 <gocar9@gmail.com>
2021-10-13 00:45:21 -0500

    added simple error handling to player

--- backend/player.py
+++ backend/player.py
@@ -4,5 +4,8 @@ from datetime import datetime
 from mutagen.mp3 import MP3
 
 def play(filePath):
-    os.system("start "+filePath)
-    time.sleep(int(MP3(filePath).info.length))
+    try:
+        os.system("start "+filePath)
+        time.sleep(int(MP3(filePath).info.length))
+    except:
+        print("Couldn't play file!")
