a0f374949dfb3ee100819208410f261caaa70678
Ryan Kruger <ryan.kruger91@gmail.com>
2021-10-13 17:04:49 -0500

    Added playlist handler. Added dummy playlists.
    Playlist handler choosese a folder at random from the playlists subdirectory. It iterates through all of the songs and calls the media player for each one.
    
    Updated constants.py with new constants.

--- backend/playlist_handler.py
+++ backend/playlist_handler.py
@@ -1,3 +1,23 @@
+import os
+from backend.constants import MEDIA_ROOT_DIRECTORY, PLAYLISTS_SUBDIRECTORY, IGNORED_FILE_EXTENSIONS
+import random
+
+test = True
+
 
 def playlist_handler():
-    pass
+    random.seed()
+    if test:
+        MEDIA_ROOT_DIRECTORY = "../media"
+    playlist_to_play = random.choice(os.listdir(MEDIA_ROOT_DIRECTORY + PLAYLISTS_SUBDIRECTORY))
+    playlist_path = MEDIA_ROOT_DIRECTORY + PLAYLISTS_SUBDIRECTORY + "/" + playlist_to_play
+    songs = [x for x in os.listdir(playlist_path) if not x.endswith(IGNORED_FILE_EXTENSIONS)]
+    if test:
+        for song in songs:
+            print(playlist_path + "/" + song)
+    for song in songs:
+        media_player(playlist_path + "/" + song)
+
+
+if __name__ == "__main__":
+    playlist_handler()
