e704c8a224ac7e872e73916f5ce8edeb4cea933c
Caleb-R0 <gocar9@gmail.com>
2021-10-12 22:02:52 -0500

    added station ID handler. dummy files are in place of the actual station IDs for now

--- backend/player.py
+++ backend/player.py
@@ -3,6 +3,6 @@ import os
 from datetime import datetime
 from mutagen.mp3 import MP3
 
-def player(filePath):
+def play(filePath):
     os.system("start "+filePath)
     time.sleep(int(MP3(filePath).info.length))
--- backend/station_id_handler.py
+++ backend/station_id_handler.py
@@ -2,15 +2,15 @@ import random, os
 import time
 from datetime import datetime
 from mutagen.mp3 import MP3
+import player
 random.seed(datetime.now())
 
-def station_id_handler(hour, min, am_pm):
+def station_id_handler(min):
     #if close to the top of the hour, play a legal id
     if min<10 or min>50:
-        ID = "backend/legal-station-IDs/"+random.choice(os.listdir("backend/legal-station-IDs"))
-        os.system("start "+ID)
-        #wait until it's done playing before doing anything else
-        time.sleep(int(MP3(ID).info.length))
+        player.play("backend/legal-station-IDs/"+random.choice(os.listdir("backend/legal-station-IDs")))
     else:
         #play a non-legal ID
-        pass
+        player.play("backend/non-legal-station-IDs/"+random.choice(os.listdir("backend/non-legal-station-IDs")))
+
+station_id_handler(30)
