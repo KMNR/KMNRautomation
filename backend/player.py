ff319437fd381e06e55964b88f385aa4afdad4fc
Caleb-R0 <gocar9@gmail.com>
2021-11-02 22:00:32 -0500

    player can now tell when it is running on Windows or Linux, and can use the correct media player for its current OS

--- backend/player.py
+++ backend/player.py
@@ -2,15 +2,21 @@ import time
 import os
 from datetime import datetime
 from mutagen.mp3 import MP3
+import platform
 
 def play(filePath):
     played=1
     try:
-        played=(os.system("start "+filePath))
-        time.sleep(int(MP3(filePath).info.length))
+        opSystem = platform.system()
+        if opSystem=="Windows":
+            played=(os.system("start "+filePath))
+        elif opSystem=="Linux":
+            played=(os.system("/usr/bin/mpv --no-terminal"+filePath))
+
+        time.sleep(int(MP3(filePath).info.length)+1)
     except:
         print("Couldn't play file!")
-    if played is 0:
+    if played == 0:
         print("File played successfully")
 
 if __name__ == "__main__":
