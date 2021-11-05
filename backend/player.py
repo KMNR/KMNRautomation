4a5fd193c31a1a1a5f0a2fe5d639e3dacbd6ad9b
Caleb-R0 <gocar9@gmail.com>
2021-11-05 16:45:43 -0500

    player can now play files with spaces in the file path

--- backend/player.py
+++ backend/player.py
@@ -19,17 +19,17 @@ def play(filePath):
     try:
         opSystem = platform.system()
         if opSystem=="Windows":
-            played=(os.system("start "+filePath))
+            played=(os.system("start "+"\"\" \""+filePath+"\""))
         elif opSystem=="Linux":
-            played=(os.system("/usr/bin/mpv --no-terminal"+filePath))
+            played=(os.system("/usr/bin/mpv --no-terminal"+"\""+filePath+"\""))
 
         #sleep can be removed for testing purposes if we don't want to wait for entire files to play
         #we have to get the length differently depending on the file type
         if(pathlib.Path(filePath).suffix==".mp3"):
-            time.sleep(int(MP3(filePath).info.length)+2)
+            time.sleep(int(MP3(filePath).info.length)+1)
         else:
             f=sf.SoundFile(filePath)
-            time.sleep(int((f.frames)/(f.samplerate)+2))
+            time.sleep(int((f.frames)/(f.samplerate))+1)
 
     except:
         print("Couldn't play file!")
@@ -39,4 +39,5 @@ def play(filePath):
         return(1)
 
 if __name__ == "__main__":
-    play("media/legalstationid/PewPewRadioSweeperID2.mp3")
+    #play("media/legalstationid/PewPewRadioSweeperID2.mp3")
+    play("media/playlists/GogoJuic3/Always On Fire.mp3")
