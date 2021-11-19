65f18b3f440b9d2d6dfdb3532a6cc4ca63c70535
Caleb-R0 <gocar9@gmail.com>
2021-11-19 16:04:35 -0600

    More work on programming logging handler

--- backend/programming_logging_handler.py
+++ backend/programming_logging_handler.py
@@ -1,13 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from constants import KELP_ID, KELP_PW
-import org.openqa.selenium.Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 def programming_logging_handler():
     browser = webdriver.Firefox()
-    browser.get('http://kelp.kmnr.org')
+    browser.get('https://'+KELP_ID+':'+KELP_PW+'@kelp.kmnr.org/show')
 
-    #form.send_keys(Keys.RETURN)
+    title_box = browser.find_element(By.CLASS_NAME, "typeahead-stationid")
+    title_box.send_keys("test1")
+
+    actions = ActionChains(browser)
+    actions.send_keys(Keys.TAB+"test2"+Keys.ENTER)
+    actions.perform()
+
+    logout_button = browser.find_element(By.XPATH, "//a[@href='/accounts/logout']")
+    logout_button.click()
 
 if __name__ == '__main__':
     programming_logging_handler()
