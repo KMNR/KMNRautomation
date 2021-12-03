439c31f61a49a9bb05d151b3d05a298fc4b3dae8
Caleb-R0 <gocar9@gmail.com>
2021-12-02 23:08:12 -0600

    Logging now checks file to see if it should log or not

--- backend/music_logging_handler.py
+++ backend/music_logging_handler.py
@@ -9,7 +9,12 @@ import time
 # Returns: None
 def music_logging_handler(playlist_path):
     browser = webdriver.Firefox()
-    browser.get('http://klap.kmnr.org/logger/new')
+    try:
+        browser.get('http://klap.kmnr.org/logger/new')
+    except:
+        print("couldn't open KLAP!")
+        return
+        
     with open(playlist_path+"/playlist.txt") as f:
         log = f.read()
 
