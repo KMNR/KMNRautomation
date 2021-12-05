579c20780f8f5c748ac9ceace3e5ba27a2386c02
amwgvf <55326926+amwgvf@users.noreply.github.com>
2021-12-05 15:27:36 -0600

    loggning button

--- frontend/app.py
+++ frontend/app.py
@@ -1,16 +1,22 @@
 #! /usr/bin/env python3
-from flask import Flask, render_template,redirect, request
+from flask import Flask, render_template, redirect, request
 from random import choice
 import os
 app = Flask(__name__)
-root_dir = "/home/ryan/Documents/automation-rework/frontend"
+root_dir = "/home/ryan/Documents/automation-rework"
+
 @app.route("/")
 def landing_page():
     funny_slider = ""
     with open("C:/Users/weste/OneDrive/Documents/Class/CS4096/automation-rework-1/frontend/static/slider_values.txt", "r") as f:
         options = f.readlines()
         funny_slider = choice(options)
-    return render_template('landing_page.html', page_name="KMNR Ultimate Music Machine", slider=funny_slider)
+
+    return render_template('landing_page.html', page_name="KMNR Ultimate Music Machine", slider=funny_slider, on_off="SHITS")
+
+@app.route("/settings")
+def settings():
+    return render_template('settings.html', page_name="Settings")
 
 @app.route("/playlists")
 def playlists():
@@ -26,7 +32,7 @@ def programming():
 
 @app.route("/logging")
 def logging():
-    return render_template('logging.html', page_name="View Logs", on_off="FUCKS")
+    return render_template('logging.html', page_name="View Logs")
 
 @app.route("/song_logs")
 def songlogs():
@@ -40,28 +46,17 @@ def errorlogs():
 def playlistlogs():
     return render_template('logging_templates/playlist_logs.html', page_name="Playlist Logs")
 
-@app.route("/toggle_logging")
+@app.route("/toggle_logging", methods=['GET', 'POST'])
 def toggle_logging():
-    global on_or_off
-    try:
-        f=open("backend/logging.txt","r")
-    except:
-        f=open("backend/logging.txt","w")
-        f.write("False")
-        f.close()
-        f=open("backend/logging.txt","r")
-    status=f.read()
-    f.close()
-    #os.system(status)
-    f=open("backend/logging.txt","w")
-    if(status=="True"):
-        f.write("False")
-        on_or_off = "Off"
-    elif(status=="False"):
-        f.write("True")
-        on_or_off = "On"
-    f.close()
-
+    global status 
+    status = ""
+    with open("C:/Users/weste/OneDrive/Documents/Class/CS4096/automation-rework-1/backend/logging.txt" ,"r") as f:
+        status = f.read()
+    with open("C:/Users/weste/OneDrive/Documents/Class/CS4096/automation-rework-1/backend/logging.txt" ,"w") as f:
+        if status == "True":
+            f.write("False")
+        else:
+            f.write("True")
     return redirect(request.referrer)
 
 if __name__ == '__main__':
