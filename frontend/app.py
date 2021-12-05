3879dcbc1c5ea77448ba72d28d598f9fc1d56adc
Caleb-R0 <gocar9@gmail.com>
2021-12-05 16:25:09 -0600

    Logging toggle button now reflects the current logging status

--- frontend/app.py
+++ frontend/app.py
@@ -8,11 +8,19 @@ root_dir = "/home/ryan/Documents/automation-rework"
 @app.route("/")
 def landing_page():
     funny_slider = ""
-    with open("C:/Users/weste/OneDrive/Documents/Class/CS4096/automation-rework-1/frontend/static/slider_values.txt", "r") as f:
+    logging_message = ""
+    with open("C:/Users/gocar/OneDrive/Documents/College/FS 21/automation-rework/frontend/static/slider_values.txt", "r") as f:
         options = f.readlines()
         funny_slider = choice(options)
 
-    return render_template('landing_page.html', page_name="KMNR Ultimate Music Machine", slider=funny_slider, on_off="SHITS")
+    with open("C:/Users/gocar/OneDrive/Documents/College/FS 21/automation-rework/backend/logging.txt", "r") as f:
+        logging_status = f.read()
+        if logging_status.strip() == "True":
+            logging_message="Off"
+        else:
+            logging_message="On"
+
+    return render_template('landing_page.html', page_name="KMNR Ultimate Music Machine", slider=funny_slider, on_off=logging_message)
 
 @app.route("/settings")
 def settings():
@@ -48,16 +56,16 @@ def playlistlogs():
 
 @app.route("/toggle_logging", methods=['GET', 'POST'])
 def toggle_logging():
-    global status 
+    global status
     status = ""
-    with open("C:/Users/weste/OneDrive/Documents/Class/CS4096/automation-rework-1/backend/logging.txt" ,"r") as f:
+    with open("C:/Users/gocar/OneDrive/Documents/College/FS 21/automation-rework/backend/logging.txt" ,"r") as f:
         status = f.read()
-    with open("C:/Users/weste/OneDrive/Documents/Class/CS4096/automation-rework-1/backend/logging.txt" ,"w") as f:
+    with open("C:/Users/gocar/OneDrive/Documents/College/FS 21/automation-rework/backend/logging.txt" ,"w") as f:
         if status == "True":
             f.write("False")
         else:
             f.write("True")
-    return redirect(request.referrer)
+    return redirect(request.referrer), status
 
 if __name__ == '__main__':
     app.run(debug=True)
