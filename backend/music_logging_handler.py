202b78afbbbeb82b7ce8110f365c167845ceaa9a
amwgvf <55326926+amwgvf@users.noreply.github.com>
2021-12-08 15:29:47 -0600

    fixing files before merge

--- backend/music_logging_handler.py
+++ backend/music_logging_handler.py
@@ -9,7 +9,7 @@ import time
 # Returns: None
 def music_logging_handler(playlist_path):
     browser = webdriver.Firefox()
-        try:
+    try:
         browser.get('http://klap.kmnr.org/logger/new')
     except:
         print("couldn't open KLAP!")
